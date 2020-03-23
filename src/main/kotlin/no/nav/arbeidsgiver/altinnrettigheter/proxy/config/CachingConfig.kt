3e8e9dac819d3149f53014280744180699aac079
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-03-23 14:20:05 +0100

    Forenkle config; skru på recording

--- src/main/kotlin/no/nav/arbeidsgiver/altinnrettigheter/proxy/config/CachingConfig.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/altinnrettigheter/proxy/config/CachingConfig.kt
@@ -8,24 +8,22 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.util.concurrent.TimeUnit
 
-
 @Configuration
 class CachingConfig {
 
+    companion object {
+        const val REPORTEES_CACHE = "reportees"
+    }
+
     @Bean
-    fun cacheManager(): CacheManager? {
-        val cacheManager = SimpleCacheManager()
-
-
-        val reporteesCache = CaffeineCache(
-                "reportees",
+    fun reporteesCache(): CaffeineCache {
+        return CaffeineCache(
+                REPORTEES_CACHE,
                 Caffeine.newBuilder()
                         .expireAfterWrite(5, TimeUnit.MINUTES)
                         .maximumSize(10000)
-                        .build<Any, Any>(),
+                        .recordStats()
+                        .build(),
                 false)
-
-        cacheManager.setCaches(listOf(reporteesCache))
-        return cacheManager
     }
 }
