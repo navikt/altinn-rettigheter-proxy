c035036c74e0afbc5dbb1396e2b3c1ee5fc845aa
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-03-24 12:21:41 +0100

    Send hele query videre til Altinn

--- src/main/kotlin/no/nav/arbeidsgiver/altinnrettigheter/proxy/altinn/AltinnClient.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/altinnrettigheter/proxy/altinn/AltinnClient.kt
@@ -2,12 +2,14 @@ package no.nav.arbeidsgiver.altinnrettigheter.proxy.altinn
 
 import no.nav.arbeidsgiver.altinnrettigheter.proxy.model.AltinnOrganisasjon
 import no.nav.arbeidsgiver.altinnrettigheter.proxy.model.Fnr
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.*
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestClientException
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 import org.springframework.web.util.UriComponentsBuilder
 import java.net.URI
 
@@ -19,24 +21,27 @@ class AltinnClient(val restTemplate: RestTemplate) {
     lateinit var altinnAPIGWApikey: String
     @Value("\${altinn.apikey}")
     lateinit var altinnApikey: String
-    @Value("\${altinn.iaweb.service.code}")
-    lateinit var iawebServiceCode: String
-    @Value("\${altinn.iaweb.service.edition}")
-    lateinit var iawebServiceEdition: String
 
-    fun hentOrgnumreDerBrukerHarEnkeltrettighetTilIAWeb(
-            fnr: Fnr,
-            serviceCode: String,
-            serviceEdition: String
+    fun hentOrgnumre(
+            query: Map<String, String>
     ): List<AltinnOrganisasjon> {
-        val uri: URI = UriComponentsBuilder.fromUriString(altinnUrl).pathSegment()
+
+        val uriBuilder = UriComponentsBuilder.fromUriString(altinnUrl).pathSegment()
                 .pathSegment("ekstern", "altinn", "api", "serviceowner", "reportees")
-                .queryParam("ForceEIAuthentication")
-                .queryParam("subject", fnr.verdi)
-                .queryParam("serviceCode", serviceCode)
-                .queryParam("serviceEdition", serviceEdition)
-                .build()
-                .toUri()
+
+        query.forEach { (key, value) ->
+            run {
+                if (value == "") {
+                    uriBuilder.queryParam(key)
+                } else {
+                    uriBuilder.queryParam(key, value)
+                }
+            }
+        }
+
+        val uri: URI = uriBuilder.build().toUri()
+
+        logger.info("uri: $uri")
 
         return try {
             val respons = restTemplate.exchange(
@@ -65,4 +70,10 @@ class AltinnClient(val restTemplate: RestTemplate) {
         return HttpEntity(headers)
     }
 
+    companion object {
+        private val logger = LoggerFactory.getLogger(AltinnClient::class.java)
+    }
 }
+
+// ekstern/altinn/api/serviceowner/reportees?ForceEIAuthentication&subject=01065500791&serviceCode=3403&serviceEdition=1
+// ekstern/altinn/api/serviceowner/reportees?ForceEIAuthentication=&subject=01065500791&serviceCode=3403&serviceEdition=1
