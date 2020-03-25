d6af334992a15e50e2d92e8710cbeff6ebfed37c
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-03-25 09:47:01 +0100

    Status skal være optional

--- src/main/kotlin/no/nav/arbeidsgiver/altinnrettigheter/proxy/model/AltinnDataModels.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/altinnrettigheter/proxy/model/AltinnDataModels.kt
@@ -14,16 +14,5 @@ data class AltinnOrganisasjon(
         @JsonProperty("OrganizationForm")
         val organizationForm: String?,
         @JsonProperty("Status")
-        val status: String
-)
-
-data class AltinnRolle(
-        @JsonProperty("RoleType")
-        private var type: String,
-        @JsonProperty("RoleDefinitionId")
-        private val definitionId: String,
-        @JsonProperty("RoleName")
-        private val name: String,
-        @JsonProperty("RoleDescription")
-        private val description: String? = null
+        val status: String?
 )
