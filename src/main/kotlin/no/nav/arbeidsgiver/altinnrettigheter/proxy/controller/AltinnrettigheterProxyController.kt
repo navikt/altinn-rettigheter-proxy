8911206eeeb757af5a121ec4031177e62e2a5bbb
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-07-29 13:46:01 +0200

    Skriv test for nytt endepunkt

--- src/main/kotlin/no/nav/arbeidsgiver/altinnrettigheter/proxy/controller/AltinnrettigheterProxyController.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/altinnrettigheter/proxy/controller/AltinnrettigheterProxyController.kt
@@ -25,20 +25,21 @@ class AltinnrettigheterProxyController(val altinnrettigheterService: Altinnretti
             @RequestHeader(value = "X-Consumer-ID", required = false) consumerId: String?,
             @RequestParam serviceCode: String, @RequestParam serviceEdition: String
     ): List<AltinnOrganisasjon> {
+
         return proxyOrganisasjoner(
                 consumerId,
                 mapOf(
                         "ForceEIAuthentication" to "",
                         "serviceCode" to serviceCode,
                         "serviceEdition" to serviceEdition,
-                        "\$filter" to "Type+ne+'Person'+and+Status+eq+'Active'",
+                        "\$filter" to "Type ne 'Person' and Status eq 'Active'",
                         "\$top" to "500",
                         "\$skip" to "0"
                 )
         )
     }
 
-    @GetMapping(value = ["v2/organisasjoner"])
+    @GetMapping("/v2/organisasjoner")
     fun proxyOrganisasjonerV2(
             @RequestHeader(value = "X-Consumer-ID") consumerId: String,
             @RequestParam serviceCode: String,
