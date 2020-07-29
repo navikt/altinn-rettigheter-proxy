bd66a12619ebb0b676952b012fcfb96e69eeac6f
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-07-29 13:48:59 +0200

    Ikke endre bruken av + i det gamle endepunktet

--- src/main/kotlin/no/nav/arbeidsgiver/altinnrettigheter/proxy/controller/AltinnrettigheterProxyController.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/altinnrettigheter/proxy/controller/AltinnrettigheterProxyController.kt
@@ -32,7 +32,7 @@ class AltinnrettigheterProxyController(val altinnrettigheterService: Altinnretti
                         "ForceEIAuthentication" to "",
                         "serviceCode" to serviceCode,
                         "serviceEdition" to serviceEdition,
-                        "\$filter" to "Type ne 'Person' and Status eq 'Active'",
+                        "\$filter" to "Type+ne+'Person'+and+Status+eq+'Active'",
                         "\$top" to "500",
                         "\$skip" to "0"
                 )
