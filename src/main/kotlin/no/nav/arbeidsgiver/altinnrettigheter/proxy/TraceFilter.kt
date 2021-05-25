8ecbe1f9512e259a4673644c7d843ae333bc24b2
Ken Gullaksen <ken.gullaksen@nav.no>
2021-05-25 10:40:18 +0200

    filter som logger alle headere i prod-fss. forsøk å finne hvem som fortsatt kaller appen der

---
+++ src/main/kotlin/no/nav/arbeidsgiver/altinnrettigheter/proxy/TraceFilter.kt
@@ -0,0 +1,21 @@
+package no.nav.arbeidsgiver.altinnrettigheter.proxy
+
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import java.io.IOException
+import javax.servlet.FilterChain
+import javax.servlet.ServletException
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+class TraceFilter : OncePerRequestFilter() {
+
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, chain: FilterChain) {
+        if (System.getenv("NAIS_CLUSTER_NAME") == "prod-fss") {
+            val headers = request.headerNames.toList().associateWith { request.getHeader(it) }
+            logger.info("${request.method} ${request.requestURI} $headers")
+        }
+        chain.doFilter(request, response)
+    }
+}
