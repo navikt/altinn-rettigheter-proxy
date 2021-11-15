a1d5db0b95795f91cf0a4d5efce86c147b452b45
Ken Gullaksen <ken.gullaksen@nav.no>
2021-11-15 15:33:06 +0100

    readiness venter på redis er ready

--- src/main/kotlin/no/nav/arbeidsgiver/altinnrettigheter/proxy/controller/HealthcheckController.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/altinnrettigheter/proxy/controller/HealthcheckController.kt
@@ -1,15 +1,41 @@
 package no.nav.arbeidsgiver.altinnrettigheter.proxy.controller
 
 import no.nav.security.token.support.core.api.Unprotected
+import org.slf4j.LoggerFactory
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @Unprotected
 @RestController
-class HealthcheckController {
-    @GetMapping("/internal/healthcheck")
-    fun healthcheck(): String {
+class HealthcheckController(
+    val redisConnectionFactory: RedisConnectionFactory,
+) {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    @GetMapping("/internal/alive")
+    fun alive(): String {
         return "OK"
+    }
+
+    @GetMapping("/internal/ready")
+    fun ready(): ResponseEntity<String> {
+        val readyReadis = try {
+            redisConnectionFactory.connection.use {
+                it.ping() != null
+            }
+        } catch (e: RuntimeException) {
+            logger.warn("redis ping feilet", e)
+            false
+        }
+
+        return if (readyReadis) {
+            ResponseEntity.ok("READY")
+        } else {
+            ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build()
+        }
     }
 
 }
