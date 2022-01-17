d4b90b65e884ab3a4c7f3f5d8b689a5164501044
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2022-01-17 10:13:54 +0100

    Marker som unhealhty hvis ting går skeis

--- src/main/kotlin/no/nav/arbeidsgiver/altinnrettigheter/proxy/maskinporten/MaskinportenClient.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/altinnrettigheter/proxy/maskinporten/MaskinportenClient.kt
@@ -8,6 +8,8 @@ import io.micrometer.core.instrument.MeterRegistry
 import no.nav.arbeidsgiver.altinnrettigheter.proxy.basedOnEnv
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.InitializingBean
+import org.springframework.boot.actuate.health.Health
+import org.springframework.boot.actuate.health.HealthIndicator
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Profile
 import org.springframework.http.HttpMethod
@@ -15,6 +17,7 @@ import org.springframework.http.MediaType
 import org.springframework.http.RequestEntity
 import org.springframework.stereotype.Component
 import org.springframework.util.LinkedMultiValueMap
+import java.lang.management.ManagementFactory
 import java.time.Duration
 import java.time.Instant
 import java.util.*
@@ -29,16 +32,16 @@ class MaskinportenClientImpl(
     val config: MaskinportenConfig,
     val meterRegistry: MeterRegistry,
     restTemplateBuilder: RestTemplateBuilder,
-): MaskinportenClient, InitializingBean {
+): MaskinportenClient, InitializingBean, HealthIndicator {
     private val logger = LoggerFactory.getLogger(javaClass)
     private val restTemplate = restTemplateBuilder.build()
     private lateinit var wellKnownResponse: WellKnownResponse
-    private val token = AtomicReference<TokenResponseWrapper?>()
+    private val tokenStore = AtomicReference<TokenResponseWrapper?>()
 
     override fun afterPropertiesSet() {
         wellKnownResponse = restTemplate.getForObject(config.wellKnownUrl, WellKnownResponse::class.java)!!
         meterRegistry.gauge(
-            "maskinporten.token.expiry.seconds", token
+            "maskinporten.token.expiry.seconds", tokenStore
         ) {
             it.get()?.expiresIn()?.seconds?.toDouble() ?: Double.NaN
         }
@@ -46,11 +49,10 @@ class MaskinportenClientImpl(
             while (true) {
                 try {
                     logger.info("sjekker om accesstoken er i ferd med å utløpe..")
-                    val value = token.get()
-                    if (value == null || value.expiresIn() < Duration.ofMinutes(10)) {
+                    val token = tokenStore.get()
+                    if (token == null || token.expiresIn() < Duration.ofMinutes(10)) {
                         val newToken = fetchNewAccessToken()
-                        token.set(newToken)
-                        logger.info("Fetched new access token. Expires in {} seconds.", newToken.expiresIn().toSeconds())
+                        tokenStore.set(newToken)
                     }
                 } catch (e: Exception) {
                     logger.error("refreshing maskinporten token failed with exception {}.", e.message, e)
@@ -58,6 +60,20 @@ class MaskinportenClientImpl(
                 Thread.sleep(Duration.ofSeconds(30).toMillis())
             }
         }.start()
+    }
+
+
+    override fun health(): Health {
+        val token = tokenStore.get()
+        if (token == null && uptime() > Duration.ofMillis(5)) {
+            return Health.down().withDetail("reason", "no token fetched since start up").build()
+        }
+
+        if (token != null && token.expiresIn() < Duration.ofMinutes(5)) {
+            return Health.down().withDetail("reason", "token about to expire").build()
+        }
+
+        return healthy
     }
 
     private fun createClientAssertion(): String {
@@ -89,36 +105,49 @@ class MaskinportenClientImpl(
         logger.info("henter ny accesstoken")
         val requestedAt = Instant.now()
 
+        val tokenResponse = restTemplate.exchange(RequestEntity
+            .method(HttpMethod.POST, wellKnownResponse.tokenEndpoint)
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .body(LinkedMultiValueMap(mapOf(
+                "grant_type" to listOf("urn:ietf:params:oauth:grant-type:jwt-bearer"),
+                "assertion" to listOf(createClientAssertion())
+            ))),
+            TokenResponse::class.java
+        ).body!!
+
+        logger.info("Fetched new access token. Expires in {} seconds.", tokenResponse.expiresInSeconds)
+
         return TokenResponseWrapper(
             requestedAt = requestedAt,
-            tokenResponse = restTemplate.exchange(RequestEntity
-                .method(HttpMethod.POST, wellKnownResponse.tokenEndpoint)
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(LinkedMultiValueMap(mapOf(
-                    "grant_type" to listOf("urn:ietf:params:oauth:grant-type:jwt-bearer"),
-                    "assertion" to listOf(createClientAssertion())
-                ))),
-                TokenResponse::class.java
-            ).body!!
+            tokenResponse = tokenResponse,
         )
     }
 
 
     private fun fetchAccessTokenCached(): TokenResponse {
-        val value = token.get()
+        val value = tokenStore.get()
         return if (value != null && value.expiresIn() >= Duration.ofSeconds(40)) {
             value.tokenResponse
         } else {
             logger.error("maskinporten access token almost expired. is refresh loop running? doing emergency fetch.")
             /* this shouldn't happen, as refresh loop above refreshes often */
             fetchNewAccessToken().also {
-                token.set(it)
+                tokenStore.set(it)
             }.tokenResponse
         }
     }
 
     override fun fetchAccessToken(): String {
         return fetchAccessTokenCached().accessToken
+    }
+
+    private fun uptime(): Duration =
+        Duration.ofMillis(
+            ManagementFactory.getRuntimeMXBean().uptime
+        )
+
+    companion object {
+        private val healthy = Health.up().build()
     }
 }
 
