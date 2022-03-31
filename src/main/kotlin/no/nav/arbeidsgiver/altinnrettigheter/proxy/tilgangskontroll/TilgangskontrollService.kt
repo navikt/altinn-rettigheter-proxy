2e4061698f298993548dc3ea083f69999f8ea74f
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2022-03-31 10:05:40 +0200

    forenkl token-sjekk

--- src/main/kotlin/no/nav/arbeidsgiver/altinnrettigheter/proxy/tilgangskontroll/TilgangskontrollService.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/altinnrettigheter/proxy/tilgangskontroll/TilgangskontrollService.kt
@@ -1,9 +1,7 @@
 package no.nav.arbeidsgiver.altinnrettigheter.proxy.tilgangskontroll
 
 import no.nav.arbeidsgiver.altinnrettigheter.proxy.model.Fnr
-import no.nav.security.token.support.core.context.TokenValidationContext
 import no.nav.security.token.support.core.context.TokenValidationContextHolder
-import no.nav.security.token.support.core.jwt.JwtTokenClaims
 import org.springframework.stereotype.Service
 
 @Service
@@ -11,39 +9,22 @@ class TilgangskontrollService(
     private val tokenValidationcontextHolder: TokenValidationContextHolder
 ) {
     companion object {
-        private const val ISSUER_SELVBETJENING = "loginservice"
         private const val ISSUER_TOKENX = "tokenx"
     }
 
     fun hentInnloggetBruker(): InnloggetBruker {
-        val context = tokenValidationcontextHolder.tokenValidationContext
-
-        context.getClaimsFor(ISSUER_SELVBETJENING)?.let { claims ->
-            return InnloggetBruker(
-                fnr = Fnr(claims.subject)
-            )
-        }
-
-        context.getClaimsFor(ISSUER_TOKENX)?.let { claims ->
-            return InnloggetBruker(
-                fnr = Fnr(claims.getStringClaim("pid"))
-            )
-        }
-
-        throw TilgangskontrollException("Finner ikke token")
+        val fnr = tokenValidationcontextHolder
+            .tokenValidationContext
+            .anyValidClaims
+            .orElseThrow { TilgangskontrollException("Finner ikke token") }
+            .getStringClaim("pid")
+            .let { Fnr(it) }
+        return InnloggetBruker(fnr = fnr)
     }
 
     fun nameOfAppCallingUs(): String? =
         tokenValidationcontextHolder
             .tokenValidationContext
-            .getClaimsFor(ISSUER_TOKENX)
+            .getClaims(ISSUER_TOKENX)
             ?.getStringClaim("client_id")
-
-    private fun TokenValidationContext.getClaimsFor(issuer: String): JwtTokenClaims? {
-        return if (hasTokenFor(issuer)) {
-            getClaims(issuer)
-        } else {
-            null
-        }
-    }
 }
