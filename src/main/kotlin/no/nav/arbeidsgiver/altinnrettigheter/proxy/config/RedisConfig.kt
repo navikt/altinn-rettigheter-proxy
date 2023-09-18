c631ab0a883fc29e9356afed9520a422a1b4fb70
Ken Gullaksen <ken.gullaksen@nav.no>
2023-09-18 23:18:43 +0200

    try switch lettuce with jedis

--- src/main/kotlin/no/nav/arbeidsgiver/altinnrettigheter/proxy/config/RedisConfig.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/altinnrettigheter/proxy/config/RedisConfig.kt
@@ -1,12 +1,11 @@
 package no.nav.arbeidsgiver.altinnrettigheter.proxy.config
 
-import io.lettuce.core.RedisURI
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import java.net.URI
+
 
 @Configuration
 class RedisConfig {
@@ -16,29 +15,19 @@ class RedisConfig {
      * nais exposes url, username and password, but RedisURI expects usename and password to be part of the url
      * https://lettuce.io/core/release/api/io/lettuce/core/RedisURI.html
      *
-     * see also org.springframework.boot.autoconfigure.data.redis.RedisProperties.url which says
+     * see org.springframework.boot.autoconfigure.data.redis.RedisProperties.url which says
      * "Connection URL. Overrides host, port, and password. User is ignored. Example: redis://user:password@example.com:6379"
      */
     @Bean
     fun redisStandalone(
-        @Value("\${spring.data.redis.url}") redisUrl: URI,
+        @Value("\${spring.data.redis.redisuri}") redisUri: URI,
         @Value("\${spring.data.redis.password}") password: String,
         @Value("\${spring.data.redis.username}") username: String,
     ) = RedisStandaloneConfiguration(
-        redisUrl.host,
-        redisUrl.port
+        redisUri.host,
+        redisUri.port
     ).apply {
         setUsername(username)
         setPassword(password)
-    }
-
-    @Bean
-    fun lettuceClientConfigurationCustomizer(
-        @Value("\${spring.data.redis.url}") redisUrl: URI,
-    ) = LettuceClientConfigurationBuilderCustomizer {
-        it.apply(RedisURI.create(redisUrl).apply {
-            // TODO: dont do this in prod
-            isVerifyPeer = false
-        })
     }
 }
