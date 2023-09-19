2bf9805f47b4e8f2eb822048c19fd557d635c649
Ken Gullaksen <ken.gullaksen@nav.no>
2023-09-19 12:29:45 +0200

    try jedis again

--- src/main/kotlin/no/nav/arbeidsgiver/altinnrettigheter/proxy/config/RedisConfig.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/altinnrettigheter/proxy/config/RedisConfig.kt
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import java.net.URI
@@ -12,6 +14,7 @@ import java.net.URI
 
 @Configuration
 class RedisConfig {
+    /*
     @Bean
     fun redisConnectionFactory(
         @Value("\${spring.data.redis.redisuri}") url: URI,
@@ -25,6 +28,26 @@ class RedisConfig {
                 setPassword(password)
                 database = redisURI.database
             }, LettuceClientConfiguration.builder().apply {
+                if (redisURI.isSsl) {
+                    useSsl()
+                }
+            }.build()
+        )
+    }*/
+
+    @Bean
+    fun redisConnectionFactory(
+        @Value("\${spring.data.redis.redisuri}") url: URI,
+        @Value("\${spring.data.redis.password}") password: String,
+        @Value("\${spring.data.redis.username}") username: String,
+    ): JedisConnectionFactory {
+        val redisURI = RedisURI.create(url)
+        return JedisConnectionFactory(
+            RedisStandaloneConfiguration(url.host, url.port).apply {
+                setUsername(username)
+                setPassword(password)
+                database = redisURI.database
+            }, JedisClientConfiguration.builder().apply {
                 if (redisURI.isSsl) {
                     useSsl()
                 }
